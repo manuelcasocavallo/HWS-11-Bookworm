b7537d53e566428d2457d54afc1da78e6b6f8597
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-13 16:36:16 +0200

    Manually added CoreData after project creation

--- HWS-11-Bookworm/HWS_11_BookwormApp.swift
+++ HWS-11-Bookworm/HWS_11_BookwormApp.swift
@@ -6,12 +6,52 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct HWS_11_BookwormApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistentContainer.viewContext)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("active")
+            case .inactive:
+                print("inactive")
+            case .background:
+                print("background")
+                saveContext()
+            default:
+                print("Something new in scenePhase")
+            }
         }
     }
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BookwormApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+            let context = persistentContainer.viewContext
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
+    
 }
