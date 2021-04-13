ee2d61fb358dba976825b333ff1acfd36a681710
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-13 15:57:33 +0200

    Initial Commit

---
+++ HWS-11-Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-11-Bookworm
+//
+//  Created by Manuel Casocavallo on 13/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-11-Bookworm/HWS_11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_11_BookwormApp.swift
+//  HWS-11-Bookworm
+//
+//  Created by Manuel Casocavallo on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
